b9b0faff7b94189151b4a9a017ee501f1c4b5ed7
Michał Romańczuk <roman@sawblade.pl>
2019-03-06 18:52:09 +0100

    done

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,13 @@
 //Twój kod
+
+const http = require('http');
+
+const srv = http.createServer((request, response) => {
+    let userAgent = request.headers['user-agent'];
+    response.setHeader('Content-type', 'text/html; charset=utf-8');
+    response.end(`<p style="margin: 0 auto; display: block; text-align: center;">Hi.<br> You're using ${userAgent}`)
+});
+
+srv.listen(3000, ()=>{
+    console.log('started listening on port 3000');
+})
